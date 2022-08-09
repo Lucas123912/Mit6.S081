63b75ceea4e7322cf91f07281673bdd6b4e437cc
Lucas <1240094386@qq.com>
2022-08-09 12:15:07 +0800

    lab3

---
+++ Lab3/sysproc.c
@@ -0,0 +1,118 @@
+#include "types.h"
+#include "riscv.h"
+#include "param.h"
+#include "defs.h"
+#include "date.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "proc.h"
+
+uint64
+sys_exit(void)
+{
+    int n;
+    if (argint(0, &n) < 0)
+        return -1;
+    exit(n);
+    return 0;  // not reached
+}
+
+uint64
+sys_getpid(void)
+{
+    return myproc()->pid;
+}
+
+uint64
+sys_fork(void)
+{
+    return fork();
+}
+
+uint64
+sys_wait(void)
+{
+    uint64 p;
+    if (argaddr(0, &p) < 0)
+        return -1;
+    return wait(p);
+}
+
+uint64
+sys_sbrk(void)
+{
+    int addr;
+    int n;
+
+    if (argint(0, &n) < 0)
+        return -1;
+
+    addr = myproc()->sz;
+    if (growproc(n) < 0)
+        return -1;
+    return addr;
+}
+
+uint64
+sys_sleep(void)
+{
+    int n;
+    uint ticks0;
+
+
+    if (argint(0, &n) < 0)
+        return -1;
+    acquire(&tickslock);
+    ticks0 = ticks;
+    while (ticks - ticks0 < n) {
+        if (myproc()->killed) {
+            release(&tickslock);
+            return -1;
+        }
+        sleep(&ticks, &tickslock);
+    }
+    release(&tickslock);
+    return 0;
+}
+
+
+#ifdef LAB_PGTBL
+int
+sys_pgaccess(void)
+{
+    uint64 start_addr;	// 测试程序中buf的地址
+    int amount;			// ........32
+    uint64 buffer;		// ........abits的地址
+    if (argaddr(0, &start_addr) < 0 || argint(1, &amount) < 0 ||
+        argaddr(2, &buffer) < 0) //获取三个参数
+        return -1;
+    struct proc* p = myproc();
+    uint64 mask = access_check(p->pagetable, start_addr); // 页表是当前进程的页表
+    if (copyout(p->pagetable, buffer, (char*)&mask, sizeof(uint64)) < 0)
+        return -1;
+    return 0;
+}
+#endif
+
+uint64
+sys_kill(void)
+{
+    int pid;
+
+    if (argint(0, &pid) < 0)
+        return -1;
+    return kill(pid);
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+uint64
+sys_uptime(void)
+{
+    uint xticks;
+
+    acquire(&tickslock);
+    xticks = ticks;
+    release(&tickslock);
+    return xticks;
+}
