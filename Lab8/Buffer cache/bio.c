9f925e17003c14c2d0f9bfccdd62a3d16c7aed96
infatuation0215 <77184053+infatuation0215@users.noreply.github.com>
2022-08-09 13:04:18 +0800

    Add files via upload

---
+++ Lab8/Buffer cache/bio.c
@@ -0,0 +1,150 @@
+// Buffer cache.
+//
+// The buffer cache is a linked list of buf structures holding
+// cached copies of disk block contents.  Caching disk blocks
+// in memory reduces the number of disk reads and also provides
+// a synchronization point for disk blocks used by multiple processes.
+//
+// Interface:
+// * To get a buffer for a particular disk block, call bread.
+// * After changing buffer data, call bwrite to write it to disk.
+// * When done with the buffer, call brelse.
+// * Do not use the buffer after calling brelse.
+// * Only one process at a time can use a buffer,
+//     so do not keep them longer than necessary.
+
+
+#include "types.h"
+#include "param.h"
+#include "spinlock.h"
+#include "sleeplock.h"
+#include "riscv.h"
+#include "defs.h"
+#include "fs.h"
+#include "buf.h"
+#define NBUCKETS 7
+#define BUCKETSZ 7
+struct bucket{
+  struct spinlock lock;
+  struct buf bufarr[BUCKETSZ];//每一个bucket 存储的buf
+};
+struct bucket bhash[NBUCKETS];
+
+int hashkey(uint key)
+{
+  return key%NBUCKETS;
+}
+
+void binit(void)
+{
+  //初始化每个锁的名称
+  uint init_stamp=ticks;
+  for(int i=0;i<NBUCKETS;i++)
+  {
+    //snprintf(name,18,"bcache",i);
+    initlock(&bhash[i].lock,"bcache");
+    for(int j=0;j<BUCKETSZ;j++)//初始化时间戳
+    {
+      bhash[i].bufarr[j].timestamp=init_stamp;
+      bhash[i].bufarr[j].bucket=i;//记录它属于哪个bucket
+    }
+  }
+}
+
+// Look through buffer cache for block on device dev.
+// If not found, allocate a buffer.
+// In either case, return locked buffer.
+static struct buf*  bget(uint dev, uint blockno)
+{
+  int key=hashkey(blockno);
+  acquire(&bhash[key].lock);//hash到对应的bucket,需要获取bucket上面的锁
+  struct buf* b;
+  for(b=&bhash[key].bufarr[0]; b<&bhash[key].bufarr[0]+BUCKETSZ; b++)
+  {
+    if(b->dev == dev && b->blockno == blockno)//如果找到该节点
+    {
+      b->refcnt++;  //增加引用数
+      b->timestamp=ticks;//更新时间戳
+      release(&bhash[key].lock);//释放bucket锁。其他进程可以访问bucket了
+      acquiresleep(&b->lock);//获取该节点的睡眠锁，准备读写
+      return b;
+    }
+  }
+  //没有找到：找时间戳最小的未使用项
+  uint minstamp=~0;
+  struct buf* min_b=0;
+  for(b=&bhash[key].bufarr[0] ; b<&bhash[key].bufarr[0]+BUCKETSZ; b++)
+  {
+    
+    if(b->timestamp<minstamp && b->refcnt==0)
+    {
+      minstamp=b->timestamp;
+      min_b=b;
+    }
+  }
+  if(min_b!=0)
+  {
+    min_b->dev = dev;
+    min_b->blockno = blockno;
+    min_b->valid = 0;
+    min_b->refcnt = 1;
+    min_b->timestamp=ticks; //记得更新时间戳
+    release(&bhash[key].lock);//释放bucket锁。其他进程可以访问bucket了
+    acquiresleep(&min_b->lock);//获取该节点的睡眠锁，准备读写
+    return min_b;
+  }
+  panic("bget: no buffers");
+}
+
+// Return a locked buf with the contents of the indicated block.
+struct buf*
+bread(uint dev, uint blockno)
+{
+  struct buf *b;
+
+  b = bget(dev, blockno);
+  if(!b->valid) {
+    virtio_disk_rw(b, 0);
+    b->valid = 1;
+  }
+  return b;
+}
+
+// Write b's contents to disk.  Must be locked.
+void
+bwrite(struct buf *b)
+{
+  if(!holdingsleep(&b->lock))
+    panic("bwrite");
+  virtio_disk_rw(b, 1);
+}
+
+// Release a locked buffer.
+// Move to the head of the most-recently-used list.
+void
+brelse(struct buf *b)
+{
+  if(!holdingsleep(&b->lock))
+    panic("brelse");
+  releasesleep(&b->lock);
+  acquire(&bhash[b->bucket].lock); //获取它所在bucket的自旋锁
+  b->refcnt--;
+  
+  release(&bhash[b->bucket].lock);
+}
+
+void
+bpin(struct buf *b) {
+  acquire(&bhash[b->bucket].lock);
+  b->refcnt++;
+  release(&bhash[b->bucket].lock);
+}
+
+void
+bunpin(struct buf *b) {
+  acquire(&bhash[b->bucket].lock);
+  b->refcnt--;
+  release(&bhash[b->bucket].lock);
+}
+
+
---
+++ Lab8/Memory allocator/kalloc.c
@@ -0,0 +1,112 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem[NCPU];
+
+void
+kinit()
+{
+  for(int i=0;i<NCPU;i++)
+  {
+      initlock(&kmem[i].lock, "kmem");
+  }
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+    kfree(p);
+}
+
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+
+  push_off();
+  int i=cpuid();
+  acquire(&kmem[i].lock);
+  r->next = kmem[i].freelist;
+  kmem[i].freelist = r;
+  release(&kmem[i].lock);
+  
+  pop_off();
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  push_off();
+  int i=cpuid();
+  acquire(&kmem[i].lock);
+  r = kmem[i].freelist;
+  if(r)
+    kmem[i].freelist = r->next;
+  release(&kmem[i].lock);
+
+  if(!r)//当前cpu对应freelist为空
+  {
+     for(int j=0;j<NCPU;j++)//从其他cpu的freelist中借用
+     {
+       if(j!=i)
+       {
+          acquire(&kmem[j].lock);
+          if(kmem[j].freelist)
+	  {
+	      r=kmem[j].freelist;
+              kmem[j].freelist=r->next; 	      
+	      release(&kmem[j].lock);
+	      break;
+	  }
+          release(&kmem[j].lock);
+       }
+     }
+  }
+  
+  pop_off();//turn on inturrupt
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
+}
